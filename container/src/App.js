ece3d40c8cfde3d1ae8189a05a9907a6e6ec513a
mhmdhalawi <mohammadhalawi93@gmail.com>
2021-11-10 19:29:51 +0200

    fix class collision for prod

--- container/src/App.js
+++ container/src/App.js
@@ -3,14 +3,21 @@ import React from 'react';
 import { MarketingApp } from './components/MarketingApp';
 import Header from './components/Header';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { createGenerateClassName, StylesProvider } from '@material-ui/styles';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
 
 export const App = () => {
   return (
     <Router>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </Router>
   );
 };
--- marketing/src/App.js
+++ marketing/src/App.js
@@ -1,15 +1,19 @@
 import React from 'react';
 
 import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
-import { StylesProvider } from '@material-ui/core/styles';
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma',
+});
+
 const App = () => {
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <Router>
           <Switch>
             <Route exact path='/pricing'>
