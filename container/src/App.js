0a5c1aa6e16d3fbeb0e64c1e9b034fa1d8b11014
mhmdhalawi <mohammadhalawi93@gmail.com>
2021-11-16 17:51:45 +0200

    add signup/signin + lazy loading

--- container/src/App.js
+++ container/src/App.js
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
 
-import { MarketingApp } from './components/MarketingApp';
 import Header from './components/Header';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { createGenerateClassName, StylesProvider } from '@material-ui/styles';
+
+const MarketingLazy = lazy(() => import('./components/MarketingApp'));
+const AuthLazy = lazy(() => import('./components/AuthApp'));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: 'co',
 });
 
-export const App = () => {
+export default () => {
   return (
-    <Router>
+    <BrowserRouter>
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header />
-          <MarketingApp />
+          <Suspense fallback={<div>Loading....</div>}>
+            <Switch>
+              <Route path="/auth" component={AuthLazy} />
+              <Route path="/" component={MarketingLazy} />
+            </Switch>
+          </Suspense>
         </div>
       </StylesProvider>
-    </Router>
+    </BrowserRouter>
   );
 };
